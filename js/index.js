679166f2174e89c41a4ad66bb09277b697543849
gesora96 <nahidahmed9933@gmail.com>
2021-12-26 01:06:46 +0600

    slick slider added

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function () {
+  'use strict';
+
+  // banner slider
+  $('#slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
