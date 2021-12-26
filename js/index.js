f29c7b39864787ef54b1088f5d01461821528c7a
gesora96 <nahidahmed9933@gmail.com>
2021-12-26 13:29:14 +0600

    slider fixed

--- js/index.js
+++ js/index.js
@@ -7,5 +7,10 @@ $(function () {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    dots: true,
+    arrows: true,
+    pauseOnHover: false,
+    nextArrow: `<i class='fas fa-chevron-right slick__next'></i>`,
+    prevArrow: `<i class='fas fa-chevron-left slick__prev'></i>`,
   });
 });
